b7529f8364b2a3e7546c3433981ef713727d6dfc
Ricardo Matias <rickmatias@msn.com>
2023-07-18 13:00:08 -0300

    refactor: removes "__libs" from git

--- __libs/util/_Index.js
+++
@@ -1,7 +0,0 @@
-const SYSTEM_ADMIN_EMAIL = "drricardomatias@clinicaunivitta.com.br";
-const MANAGER_TRELLO_USERNAME = "drricardomatias";
-
-const SHOW_LOGS = true;
-const EXECUTION_TIME_OUT = 30; //minutes
-
-const Util = {};
